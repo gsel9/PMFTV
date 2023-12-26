4e063f79e7c0c3c5ececa8d2e17f387d11afd6ef
Severin Langberg <langberg91@gmail.com>
2023-12-26 23:41:18 +0100

    added wcmc material

--- src/lmc/__init__.py
+++ src/lmc/__init__.py
@@ -1,11 +1,13 @@
 from .cmc import CMC
 from .lmc import LMC
+from .wcmc import WCMC, WCMCADMM
 
 __all__ = [
     "LMC",
     "CMC",
     "SCMC",
     "WCMC",
+    "WCMCADMM",
     "TVMC",
     "LarsMC",
     "MatrixCompletionBase",
